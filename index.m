dc2b7af9a007d00cc477fd344547c5d5e3431173
HagarMahmoud <enghagar1995@hotmail.com>
2016-04-11 21:02:34 +0200

    firstPage

---
+++ index.m
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>linkedPage</title>
+</head>
+<body>
+<div align="center">
+<form  name="first">
+<br>
+<h3><font color="blue" face="Tekton Pro">UserName</font></h3>
+<input type="text" name="user">
+<br><br>
+<h3><font color="blue" face="Tekton Pro">Password</font></h3>
+<input name="password" size="20">
+<br>
+<br>
+<br>
+<input type="button" value="Sign in" onClick="location. href='www.yahoo.com';">
+<br>
+<br>
+<h3><font color="blue" face="Tekton Pro">Your Comment</font></h3>
+<textarea rows="5" cols="20" ></textarea>
+<br><br><br>
+<br>
+</form>
+</div> 
+</body>
+</html>
