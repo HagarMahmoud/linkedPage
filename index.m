d0790fa1a83de39cfe1c065e7578ec6af8bb493b
HagarMahmoud <enghagar1995@hotmail.com>
2016-04-11 23:24:31 +0200

    first

--- index.m
+++ index.m
@@ -1,20 +1,35 @@
 <html>
 <head>
 <title>linkedPage</title>
+<script type="text/javascript">
+function check(form)
+{
+ 
+ if(form.user.value == "user" && form.password.value == "pswrd")
+  {
+    window.open('www.facebook.com')
+  }
+ else
+ {
+   window.alert("Error Password or Username Try again")
+  }
+}
+</script>
 </head>
 <body>
 <div align="center">
-<form  name="first">
+<form  name="login">
 <br>
 <h3><font color="blue" face="Tekton Pro">UserName</font></h3>
 <input type="text" name="user">
 <br><br>
 <h3><font color="blue" face="Tekton Pro">Password</font></h3>
-<input name="password" size="20">
+<input type="password"  name="password" size="20">
+
 <br>
 <br>
 <br>
-<input type="button" value="Sign in" onClick="location. href='www.yahoo.com';">
+<input type="button" value="Log in" onClick="check(this.form)">
 <br>
 <br>
 <h3><font color="blue" face="Tekton Pro">Your Comment</font></h3>
@@ -23,5 +38,6 @@
 <br>
 </form>
 </div> 
+
 </body>
 </html>
